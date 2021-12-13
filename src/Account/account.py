96212e654586b80def6cef6b7c0736d24b36152c
elihu <elisabeth.humblet@ulb.be>
2021-12-13 10:08:52 +0100

    First try - newAccount or login

---
+++ src/Account/account.py
@@ -0,0 +1,17 @@
+class Account:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    # store username and password in database
+
+    def newAccount(self):
+        print("You don't have an account yet")
+        self.username = input("Username : ")
+        self.password = input("Your password : ")
+
+    def login(self):
+        # account already in DB
+        self.username = input("Username : ")
+        self.password = input("Your password : ")
