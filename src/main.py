2e396ca7e698292be627c579a5cf2c6c75dfb4b2
Jeanne 6H <jeanne.szpirer@ulb.be>
2021-12-14 16:40:06 +0100

    Localhost dans main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+from src.Messaging.ServerConnection import ServerConnection
+
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 6666
 separator = "<SEP>"
 
 def main():
+    serverconn = ServerConnection("localhost")
     print("Welcome in BPost-Client")
 
 if __name__ == '__main__' :
