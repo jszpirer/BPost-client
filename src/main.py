57e40d81c6730df40dcf91a9872e36cde0d5ee42
Alexandre Flachs <alexandre.flachs@ulb.be>
2021-12-19 22:53:39 +0100

    Client-server connection is secured and ready to use

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 import asyncio
-from Messaging.ServerConnection import ServerConnection
+from ServerConnection import ServerConnection
 
 SERVER_HOST = "localhost"
 SERVER_PORT = 8000
 separator = "<SEP>"
 
 
+async def start_menu(server_conn):
+    print("Starting menu")
+    # Todo
+    server_conn.send_message("Test message")
+
+
 async def main():
-    serverconn = ServerConnection("localhost")
-    await serverconn.receiveMessage()
     print("Welcome in BPost-Client")
+    server_conn = ServerConnection(SERVER_HOST, SERVER_PORT)
+    await asyncio.gather(
+        server_conn.start(),
+        start_menu(server_conn),
+    )
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
