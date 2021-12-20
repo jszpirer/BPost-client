5953b4bc91b94a8d7b1635010ad514a41743069f
elihu <elisabeth.humblet@ulb.be>
2021-12-20 21:57:24 +0100

    confirmation Server

--- src/Menus/reactMenus.py
+++ src/Menus/reactMenus.py
@@ -3,11 +3,12 @@ from src.Messaging.message import *
 from src.Menus.printMenus import *
 from src.asyncronous_functions import *
 
-def reactTopMenu(option, connection):
+
+async def reactTopMenu(option, connection):
     if option == 1:  # create account
-        createAccount(connection)
+        acc = await createAccount(connection)
     elif option == 2:  # login
-        acc = login(connection)
+        acc = await login(connection)
     else:
         print("Invalid option. Please enter a number between 1 and 2")
     printActionMenu()
@@ -18,10 +19,10 @@ def reactTopMenu(option, connection):
             break
         except:
             print("Wrong input. Please enter a number")
-    reactActionMenu(action, acc, connection)
+    await reactActionMenu(action, acc, connection)
 
 
-def createAccount(connection):
+async def createAccount(connection):
     print("You don't have an account yet. Please enter a username and then enter a password")
     username = await ainput("Username : ")
     password = await ainput("Your password : ")
@@ -30,74 +31,76 @@ def createAccount(connection):
         password = hash(password)
     else:
         print("Passwords don't match")
-        # TODO : passwords don't match
+        return await createAccount(connection)
     toServ = [username, password]  # info to send to the server
-    if authenticate("newAccount", toServ, connection):
+    if confirmationServ("2", connection):
         print("You have successfully created you BPost Account !")
-        acc = login(connection)
+        acc = await login(connection)
         return acc
     else:
         print("This username already exists. Please choose a different username")
-        return createAccount(connection)
+        return await createAccount(connection)
 
 
-def login(connection):
+async def login(connection):
     print("Welcome back to the BPost Messaging App !")
     print("Please enter your username and then enter your password")
     username = await ainput("Username : ")
     password = await ainput("Your password : ")
     password = hash(password)
     toServ = [username, password]  # info to send to the server !! formatage
-    if authenticate("login", toServ, connection):
+    if confirmationServ("1", connection):
         print("Login successful")
         acc = Account(username, password)
         return acc
     else:
         print("Your username or password is incorrect. Please try again")
-        return login(connection)
+        return await login(connection)
 
 
-
-def reactActionMenu(option, acc, connection):
+async def reactActionMenu(option, acc, connection):
     if option == 1:  # send a message
-        toServ = sendMessage(acc, connection)
+        toServ = await sendMessage(acc, connection)
     elif option == 2:  # add a contact to the contact list
-        addContact(acc, connection)
+        await addContact(acc, connection)
     else:
         print("Invalid option. Please enter a number between 1 and 2")
     return toServ
 
 
-def sendMessage(acc, connection):
+async def sendMessage(acc, connection):
     print("Here is your contact list : ", acc.contacts)
     recipient = await ainput("Send to : ")
     content = await ainput("Write here : ")
     toServ = recipient
-    if authenticate("sendMessage", toServ, connection):
+    if confirmationServ("0", connection):
         mess = Message(acc.getUsername(), recipient, content)
         return mess
     else:
         print("This person does not exist in our database. Please try again.")
-        return sendMessage(acc, connection)
+        return await sendMessage(acc, connection)
 
 
-
-def addContact(acc, connection):
+async def addContact(acc, connection):
     contact = await ainput("What is the username of the contact you would like to add to your list : ")
     toServ = contact
-    if authenticate("add Contact to list", toServ, connection):  # contact exists?
+    if confirmationServ("4", connection):
         acc.newContact(contact)
         print("Contact successfully added to your list")
     else:
         print("This person does not exist in our database. Please try again.")
-        addContact(acc, connection)
+        await addContact(acc, connection)
 
 
-def authenticate(action, toServer, connection):
-    # format with the action !
-    connection.send_message(toServer)
-    state = connection.receive_message()
-    if state:
-        return True
-    else:
-        return False
+def confirmationServ(action, connection):
+    """checks the answer of the server to the action request (username correct, etc)"""
+    configMessages = connection.getConfigMessages()
+    read = False
+    while read == False:
+        for i in range(len(configMessages)):
+            if configMessages[i][0] == action:  # checks the action value
+                confirmation = configMessages[i][1]
+                print("ca marche")
+                read = True
+    return confirmation
+
